b9a0280ccf2a65352d596b068ef191ef4dd334ab
Astrix1234 <130665627+Astrix1234@users.noreply.github.com>
2023-06-09 13:04:18 +0200

    animations corrections

--- js/script.js
+++ js/script.js
@@ -11,14 +11,20 @@ function openMenu() {
   openBtn.classList.add('is-hidden');
   clouseBtn.classList.remove('is-hidden');
   openFirstElement.classList.add('open-agency');
+  openFirstElement.classList.remove('clouse-agency');
   openSecondElement.classList.add('open-portfolio');
+  openSecondElement.classList.remove('clouse-portfolio');
   openThirdElement.classList.add('open-contact');
+  openThirdElement.classList.remove('clouse-contact');
 }
 
 function clouseMenu() {
   openBtn.classList.remove('is-hidden');
   clouseBtn.classList.add('is-hidden');
   openFirstElement.classList.remove('open-agency');
+  openFirstElement.classList.add('clouse-agency');
   openSecondElement.classList.remove('open-portfolio');
+  openSecondElement.classList.add('clouse-portfolio');
   openThirdElement.classList.remove('open-contact');
+  openThirdElement.classList.add('clouse-contact');
 }
